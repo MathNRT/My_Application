cb2735aa7b5d995f4e51f1701a467126ba30caa2
Aluno <aluno@LI.LOCAL>
2022-03-25 22:05:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/my_application/FormularioPersonagemActivity.java
@@ -0,0 +1,14 @@
+package com.example.my_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+    }
+}
